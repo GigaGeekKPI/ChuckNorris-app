9becf08c821018d2b3f3e6ea0197568b20e05be2
GigaGeekKPI <life.x.dream.programmer@gmail.com>
2020-05-24 20:39:54 +0300

    Updates

---
+++ precache-manifest.8c95ef1f1d893807574ed25376a99a7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82deba57ccd92ab787d4d89c3cf3f2f6",
+    "url": "/ChuckNorris-app/index.html"
+  },
+  {
+    "revision": "6eeebdeb5bf9d3f23b4c",
+    "url": "/ChuckNorris-app/static/css/main.7d948ad0.chunk.css"
+  },
+  {
+    "revision": "056e2ed30a9175a72df9",
+    "url": "/ChuckNorris-app/static/js/2.683221ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ChuckNorris-app/static/js/2.683221ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eeebdeb5bf9d3f23b4c",
+    "url": "/ChuckNorris-app/static/js/main.586dac07.chunk.js"
+  },
+  {
+    "revision": "fdc6fcaa8551ab101ff1",
+    "url": "/ChuckNorris-app/static/js/runtime-main.ee314b6d.js"
+  }
+]);
